e4c9409e186b81bd17ad1c4d941733eeabbd6a0d
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-03-21 21:24:36 -0400

    Added Greenmonster::MlbGame#update_with_line_score_data

--- lib/greenmonster/model_extensions/mlb_game.rb
+++ lib/greenmonster/model_extensions/mlb_game.rb
@@ -1,11 +1,79 @@
 module Greenmonster
 	module MlbGame
-    
     def to_s
       "Game #{self.game_pk}: #{self.game_id[15,6]} @ #{self.game_id[22,6]}"
     rescue
       "Game #{self.game_pk}: (Game ID not valid.)"
     end
+    
+    def update_with_line_score_data(line_score)
+      # Set Game Primary Key
+      self.game_pk = line_score.attributes['game_pk'].value.to_i
+      self.game_id = if line_score.has_attribute?('gameday_link')
+        line_score.attribute('gameday_link').value.gsub('_','')
+      elsif line_score.has_attribute?('id') 
+        line_score.attribute('id').value.gsub('/','').gsub('-','')
+      else
+        raise "Unknown game_id, need new method for game."
+      end
+    
+      self.game_num = self.game_id[20].to_i
+      
+      # Set date
+      self.date = case
+      when line_score.attributes['id']
+        Date.new(
+          line_score.attributes['id'].value[0,4].to_i,
+          line_score.attributes['id'].value[5,2].to_i,
+          line_score.attributes['id'].value[8,2].to_i
+        )
+      when line_score.attributes['gameday_link']
+        Date.new(
+          line_score.attributes['gameday_link'].value[0,4].to_i,
+          line_score.attributes['gameday_link'].value[5,2].to_i,
+          line_score.attributes['gameday_link'].value[8,2].to_i
+        )
+      else
+        Date.new(0000,00,00)
+      end
+    
+      # Set start time
+      if line_score.attributes['time'] and line_score.attributes['time'].value.include?(':')
+        self.game_time = Time.parse("#{self.date} #{line_score.attributes['time'].value} #{line_score.attributes['ampm']}")
+      else
+        self.game_time = Time.new(self.date.year,self.date.month,self.date.day,19,30)
+      end
+    
+      # Set game status and type
+      self.game_status = line_score.attributes['ind'].value if line_score.attributes['ind']
+      self.game_type = line_score.attributes['game_type'].value if line_score.attributes['game_type']
+    
+      # Set venue information
+      if line_score and line_score.attributes["venue"] and line_score.attributes["venue"].value and line_score.attributes['venue_id']
+        self.venue_id = line_score.attributes['venue_id'].value.to_i
+        self.game_venue = line_score.attributes['venue'].value
+      end
+    
+      # Set leagues
+      self.home_league_id = line_score.attributes["home_league_id"].value.to_i if line_score.attributes["home_league_id"]
+      self.away_league_id = line_score.attributes["away_league_id"].value.to_i if line_score.attributes["away_league_id"]
+  
+      # Set divisions
+      self.home_division = line_score.attributes["home_division"].value if line_score.attributes["home_division"]
+      self.away_division = line_score.attributes["away_division"].value if line_score.attributes["away_division"]
+
+      # Set sport codes
+      self.home_sport_code = line_score.attributes["home_sport_code"].value if line_score.attributes["home_sport_code"]
+      self.away_sport_code = line_score.attributes["away_sport_code"].value if line_score.attributes["away_sport_code"]
+      self.sport_code = self.home_sport_code
+    
+      # Set teams
+      self.home_team_id = line_score.attributes["home_team_id"].value.to_i if line_score.attributes["home_team_id"]
+      self.away_team_id = line_score.attributes["away_team_id"].value.to_i if line_score.attributes["away_team_id"]
+    
+      return self.save
+    end
+    
     
     def self.included(k)
       def k.create_from_gameday_xml_game(gid, args = {})
---
+++ test/test_update_mlb_game_with_linescore_data.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'greenmonster'
+require 'supermodel'
+
+class GamedayGame < SuperModel::Base
+  include Greenmonster::MlbGame
+  
+  def self.find_or_initialize_by_id(id_number)
+    begin
+      a = self.find(id_number)
+    rescue
+    end
+    
+    if a.nil?
+      return self.find_or_create_by_id(id_number)
+    else
+      return a
+    end
+  end
+end
+
+class TestUpdateMlbGameWithLinescore < MiniTest::Unit::TestCase
+  def test_update_with_linescore_data
+    g = GamedayGame.new
+    g.update_with_line_score_data(Greenmonster::Parser.load_line_score('gid_2011_07_04_tormlb_bosmlb_1'))
+    
+    assert_equal 288186, g.game_pk
+    assert_equal 111, g.home_team_id
+    assert_equal 141, g.away_team_id
+    assert_equal 1, g.game_num
+    assert_equal '20110704tormlbbosmlb1', g.game_id
+    assert_equal Date.new(2011,7,4), g.date
+    assert_equal 'F', g.game_status
+    assert_equal Time.new(2011,7,4,13,35), g.game_time
+    assert_equal 'R', g.game_type
+    assert_equal 'Fenway Park', g.game_venue
+    assert_equal 3, g.venue_id
+    assert_equal 'mlb', g.sport_code
+    assert_equal 103, g.home_league_id
+    assert_equal 103, g.away_league_id
+  end
+
+  def setup
+    Greenmonster.set_games_folder('./test/games')
+  end
+end
