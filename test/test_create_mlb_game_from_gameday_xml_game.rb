5445cd2f45412997401bc3eec9c001c388293286
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-03-21 20:56:52 -0400

    Testing failure.

--- test/test_create_mlb_game_from_gameday_xml_game.rb
+++ test/test_create_mlb_game_from_gameday_xml_game.rb
@@ -24,6 +24,10 @@ class TestCreateMlbGameFromGamedayXMLGame < MiniTest::Unit::TestCase
     GamedayGame.create_from_gameday_xml_game('gid_2011_07_04_tormlb_bosmlb_1')
     assert_equal 1, GamedayGame.all.count
   end
+
+  def test_fail_gracefully_if_game_does_not_have_data
+    refute GamedayGame.create_from_gameday_xml_game('gid_2011_07_04_tormlb_bosmlb_8')
+  end
   
   def setup
     Greenmonster.set_games_folder('./greenmonster_test_games')
