853650208ebd72808c86c0521ebe2924315eeba2
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-03-22 22:50:31 -0400

    Cleanup of test cases.

--- test/test_greenmonster_spider.rb
+++ test/test_greenmonster_spider.rb
@@ -54,33 +54,29 @@ class GreenmonsterSpiderTest < MiniTest::Unit::TestCase
     
     assert_equal 4, Dir.entries(@local_test_data_location + '/asx/year_2011/month_09/day_12/').count
     %w(linescore.xml boxscore.xml players.xml eventLog.xml inning).each do |f|
-      assert Dir.entries(@local_test_data_location + '/asx/year_2011/month_09/day_01/gid_2011_09_12_staasx_aubasx_1/').include? f
+      assert Dir.entries(@local_test_data_location + '/asx/year_2011/month_09/day_12/gid_2011_09_12_staasx_aubasx_1/').include? f
     end
   end
   
   def test_pull_games_prior_to_2008
     Greenmonster::Spider.pull_game('gid_2007_04_15_detmlb_tormlb_1', {:games_folder => @local_test_data_location, :print_games => false})
-    
     assert_equal 12, Dir.entries(@local_test_data_location + '/mlb/year_2007/month_04/day_15/gid_2007_04_15_detmlb_tormlb_1/inning/').count
   end
   
   def test_pull_all_sport_codes_for_day
     Greenmonster::Spider.pull_day({:all_sport_codes => true, :print_games => false, :games_folder => @local_test_data_location, :date => Date.new(2011,9,13)})
-  
     assert_equal 8, Dir.entries(@local_test_data_location).count
   end
   
   def test_pull_days
     Greenmonster::Spider.pull_days((Date.new(2011,10,9)..Date.new(2011,10,10)), {:print_games => false, :games_folder => @local_test_data_location})
-    
     assert_equal 4, Dir.entries(@local_test_data_location + '/mlb/year_2011/month_10/').count
     assert_equal 6, Dir.entries(@local_test_data_location + '/mlb/year_2011/month_10/day_09').count
   end
   
   def test_pull_day_with_default_folder_location
-    Greenmonster.set_games_folder(@local_test_data_location)
+    Greenmonster.set_games_folder @local_test_data_location
     Greenmonster::Spider.pull_day({:date => Date.new(2011,7,4), :print_games => false})
-    
     assert_equal 288186, Nokogiri::XML(open(@local_test_data_location + '/mlb/year_2011/month_07/day_04/gid_2011_07_04_tormlb_bosmlb_1/boxscore.xml')).search('boxscore').first.attribute('game_pk').value.to_i
   end
   
@@ -96,13 +92,11 @@ class GreenmonsterSpiderTest < MiniTest::Unit::TestCase
   
   def test_local_file_not_created_if_remote_file_does_not_exist
     Greenmonster::Spider.pull_game('gid_2011_07_01_xxxmlb_yyymlb_1', {:games_folder => @local_test_data_location, :print_games => false})
-    
     assert_equal 3, Dir.entries(@local_test_data_location + '/mlb/year_2011/month_07/day_01/gid_2011_07_01_xxxmlb_yyymlb_1').count
   end
   
   def test_sport_code_argument_as_array
     Greenmonster::Spider.pull_day({:date => Date.new(2011,9,16), :sport_code => ['mlb','rok'], :print_games => false, :games_folder => @local_test_data_location})
-    
     assert_equal 4, Dir.entries(@local_test_data_location).count
   end
   
--- test/test_greenmonster_traversal.rb
+++ test/test_greenmonster_traversal.rb
@@ -13,9 +13,7 @@ class GreenmonsterTraversalTest < MiniTest::Unit::TestCase
     end
   end
   
-  def test_traverse_folders_for_date
-    FileUtils.mkdir_p Greenmonster.games_folder + 'mlb' + 'year_2012' + 'month_03' + 'day_27' + 'gid_2012_03_27_aaamlb_aabmlb_1'
-    
+  def test_traverse_folders_for_date    
     assert_block do
       Greenmonster.traverse_folders_for_date(Date.new(2012,3,27), {:true_test => true, :false_test => false}) do |g,a|
         g == 'gid_2012_03_27_aaamlb_aabmlb_1' and a == {:true_test => true, :false_test => false}
@@ -23,9 +21,7 @@ class GreenmonsterTraversalTest < MiniTest::Unit::TestCase
     end
   end
   
-  def test_traverse_folders_for_non_mlb_sport_code
-    FileUtils.mkdir_p Greenmonster.games_folder + 'tst' + 'year_2012' + 'month_03' + 'day_27' + 'gid_2012_03_27_aaatst_aabtst_1'
-    
+  def test_traverse_folders_for_non_mlb_sport_code    
     assert_block do
       Greenmonster.traverse_folders_for_date(Date.new(2012,3,27), {:sport_code => 'tst'}) do |g,a|
         g == 'gid_2012_03_27_aaatst_aabtst_1'
