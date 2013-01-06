5d12b925a8e8c2ec2dd7286484439474063d7aef
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-01-06 11:12:01 -0500

    Removed whitespace.

--- lib/greenmonster.rb
+++ lib/greenmonster.rb
@@ -10,19 +10,19 @@ require 'date'
 
 module Greenmonster
   @@games_folder = nil
-  
+
   ##
   # Set the default folder to which games
   # are saved after being downloaded from
   # the server.
   #
-  # Example: 
-  #   => Greenmonster.set_games_folder("/Users/geoff/game_data")
+  # Example:
+  #  => Greenmonster.set_games_folder("/Users/geoff/game_data")
   #
   # Arguments:
-  #   location: (String)
+  #  location: (String)
   #
-  
+
   def self.set_games_folder(location)
     @@games_folder = Pathname.new(location)
   end
@@ -31,57 +31,53 @@ module Greenmonster
   # Return the default games folder location
   #
   # Example:
-  #   >> Greenmonster.set_games_folder("/Users/geoff/game_data")
-  #   >> Greenmonster.games_folder
-  #   => #<Pathname:/Users/geoff/game_data> 
+  #  >> Greenmonster.set_games_folder("/Users/geoff/game_data")
+  #  >> Greenmonster.games_folder
+  #  => #<Pathname:/Users/geoff/game_data>
 
   def self.games_folder
     @@games_folder
   end
-  
+
   def self.format_date_as_folder(date)
     date.strftime("year_%Y/month_%m/day_%d")
   end
-  
+
   ##
   # Walk the dates in a range of dates, and execute whatever
-  # methods on the date and argument set specified. Used when 
+  # methods on the date and argument set specified. Used when
   # processing games and players.
   #
   #
-  
+
   def self.traverse_dates(range = (Date.today..Date.today), args = {})
-     range.each do |day|
-        yield day,args
-     end
+    range.each do |day|
+      yield day,args
+    end
   end
-  
+
   ##
   # Walk the game folders in a range of dates, and execute whatever
-  # methods on the game folder and argument set specified. Used when 
+  # methods on the game folder and argument set specified. Used when
   # processing games and players.
   #
   #
-  
-  def self.traverse_folders_for_date(day,args = {})
-     begin
-        folders = Dir.entries(Pathname.new(args[:games_folder] || @@games_folder) + (args[:sport_code] || 'mlb') + self.format_date_as_folder(day))
-     rescue StandardError => boom
-        puts "No files for #{day.to_s}."
-     end
 
-     unless folders.nil?
-        folders.sort.each do |gdir|
-           if gdir[0,3] == 'gid' and gdir[-4,4] != "_bak"
-              yield gdir, args
-           end
+  def self.traverse_folders_for_date(day,args = {})
+    begin
+      folders = Dir.entries(Pathname.new(args[:games_folder] || @@games_folder) + (args[:sport_code] || 'mlb') + self.format_date_as_folder(day))
+    rescue StandardError => boom
+      puts "No files for #{day.to_s}."
+    end
+
+    unless folders.nil?
+      folders.sort.each do |gdir|
+        if gdir[0,3] == 'gid' and gdir[-4,4] != "_bak"
+          yield gdir, args
         end
-     end
+      end
+    end
   end
 end
 
 require 'greenmonster/spider'
-require 'greenmonster/model_extensions/player'
-require 'greenmonster/model_extensions/mlb_game'
-require 'greenmonster/model_extensions/mlb_probable_pitcher'
-require 'greenmonster/parser'
--- lib/greenmonster/spider.rb
+++ lib/greenmonster/spider.rb
@@ -1,17 +1,17 @@
 # The Gameday XML Spider utility
 module Greenmonster::Spider
   include HTTParty
-  
+
   ##
-  # Pull Gameday XML files for a given game, specified by the game 
+  # Pull Gameday XML files for a given game, specified by the game
   # ID. If date and sport code are not specified as options, these
   # values are guessed from the game ID string using the home team's
   # sport code and the date from the scheduled date values in the game
   # ID.
   #
   # Example:
-  #   >> Gameday::Spider.pull_game('',{:games_folder => })
-  
+  #  >> Gameday::Spider.pull_game('',{:games_folder => })
+
   def self.pull_game(game_id,args = {})
     args = {
       :date => args[:date] || Date.new(game_id[4,4].to_i, game_id[9,2].to_i, game_id[12,2].to_i),
@@ -20,29 +20,29 @@ module Greenmonster::Spider
       :games_folder => Greenmonster.games_folder
     }.merge(args)
     raise "Games folder location required." if args[:games_folder].nil?
-    
+
     args[:games_folder] = Pathname.new(args[:games_folder])
-    
+
     puts game_id if args[:print_games]
-        
+
     paths = {
       :localGameFolder => args[:games_folder] + args[:sport_code] + format_date_as_folder(args[:date]) + game_id,
       :mlbGameFolder => "#{gameday_league_and_date_url(args)}/#{game_id}/"
     }
-        
+
     FileUtils.mkdir_p paths[:localGameFolder] + 'inning'
 
     begin
       # Always copy linescore first. If we can't get this
       # data, all other game data is useless.
       copy_gameday_xml('linescore.xml',paths)
-      
+
       if args[:date].year > 2007
         ['inning_all.xml', 'inning_hit.xml'].each do |file|
           copy_gameday_xml(file,paths)
         end
       else
-        # Iterate through the inning files, but skip inning 
+        # Iterate through the inning files, but skip inning
         # files numbered 0 (some bad spring training data).
         # Necessary for games prior to 2008 because there is
         # no inning_all.xml file in older games.
@@ -51,45 +51,45 @@ module Greenmonster::Spider
         end
       end
 
-      # Copy base data files 
+      # Copy base data files
       # (if inning data wasn't there, this gets skipped)
       ['boxscore.xml','eventLog.xml','players.xml'].each do |file|
         copy_gameday_xml(file,paths)
       end
     rescue StandardError => bang
-      puts "Unable to download some data for #{game_id}"  
+      puts "Unable to download some data for #{game_id}"
     end
-    
+
     return game_id
   end
-    
+
   ##
-  # Pull Gameday XML files for a given date. Default options for 
-  # the spider are to pull games with sport_code of 'mlb' (games 
-  # played by MLB games rather than MiLB teams or foreign teams) 
-  # and to pull games on the current date. 
+  # Pull Gameday XML files for a given date. Default options for
+  # the spider are to pull games with sport_code of 'mlb' (games
+  # played by MLB games rather than MiLB teams or foreign teams)
+  # and to pull games on the current date.
   #
   # Example:
-  #   # Pull games from July 4, 2011
-  #   >> Gameday::Spider.pull_day({:date => Date.new(2011,7,1), :games_folder => '/Users/geoff/games'})
+  #  # Pull games from July 4, 2011
+  #  >> Gameday::Spider.pull_day({:date => Date.new(2011,7,1), :games_folder => '/Users/geoff/games'})
   #
   # Arguments:
-  #   args: (Hash)
+  #  args: (Hash)
   #
-    
+
   def self.pull_day(args = {})
     args = {
       :date => Date.today,
       :sport_code => 'mlb',
     }.merge(args)
-    
+
     # If we want all sport codes, set up the array.
     if args[:all_sport_codes]
       args[:sport_codes] = %w(aaa aax afa afx asx bbc fps hsb ind int jml nae naf nas nat naw oly rok win)
     else
       args[:sport_codes] = [args[:sport_code] || 'mlb'].flatten
     end
-    
+
     # Iterate through every hyperlink on the page.
     # These links represent the individual game folders
     # for each date. Reject any links that aren't to game
@@ -97,8 +97,8 @@ module Greenmonster::Spider
     # folders.
     args[:sport_codes].each do |sport_code|
       args[:sport_code] = sport_code
-      (Nokogiri::XML(self.get(gameday_league_and_date_url(args)))/"a").reject{|l| l.attribute('href').value[0,4] != "gid_" or l.attribute('href').value[-5,4] == "_bak"}.each do |e|      
-        self.pull_game(e.attribute('href').value.gsub('/',''),args) 
+      (Nokogiri::XML(self.get(gameday_league_and_date_url(args)))/"a").reject{|l| l.attribute('href').value[0,4] != "gid_" or l.attribute('href').value[-5,4] == "_bak"}.each do |e|
+        self.pull_game(e.attribute('href').value.gsub('/',''),args)
       end
     end
 
@@ -110,18 +110,18 @@ module Greenmonster::Spider
   # passes arguments like games_folder location on to Spider.pull.
   #
   # Example:
-  #   # Pull all games in MLB in July 2011
-  #   >> Gameday::Spider.pull_days(Date.new(2011,7,1)..Date.new(2011,7,31), {:games_folder => '/Users/geoff/games'})
+  #  # Pull all games in MLB in July 2011
+  #  >> Gameday::Spider.pull_days(Date.new(2011,7,1)..Date.new(2011,7,31), {:games_folder => '/Users/geoff/games'})
   #
   # Arguments:
-  #   range: (Range)
-  #   args: (Hash)
+  #  range: (Range)
+  #  args: (Hash)
 
   def self.pull_days(range,args = {})
     range.each {|day| self.pull_day(args.merge({:date => day}))}
   end
-    
-    private
+
+  private
     ##
     # Return the Gameday URL for a given sport_code
     # and date. Argument must include :date, but
@@ -129,46 +129,46 @@ module Greenmonster::Spider
     # specified.
     #
     # Arguments:
-    #   args: (Hash)
+    #  args: (Hash)
     #
     def self.gameday_league_and_date_url(args = {})
       raise "Date required." unless args[:date]
       args[:sport_code] ||= 'mlb'
-    
+
       "http://gd2.mlb.com/components/game/#{args[:sport_code]}/#{format_date_as_folder(args[:date])}"
     end
-    
+
     ##
     # Copy XML files from the Gameday severs to your
     # local machine for parsing and analysis. This method
-    # is used by Spider pull class methods to put files on 
-    # your local machine in a similar layout to the one used by 
+    # is used by Spider pull class methods to put files on
+    # your local machine in a similar layout to the one used by
     # Gameday. The paths argument gets built by Spider.pull
     # during the pull process.
     #
-    # Arguments: 
-    #   file_name: (String)
-    #   paths: (Hash)
-  
+    # Arguments:
+    #  file_name: (String)
+    #  paths: (Hash)
+
     def self.copy_gameday_xml (file_name,paths)
       download = self.get(paths[:mlbGameFolder] + "#{file_name =~ /inning/ ? 'inning/' : ''}" + file_name).body.force_encoding("ISO-8859-1").encode("UTF-8")
       unless download.include?('404 Not Found')
         open(paths[:localGameFolder] + (file_name =~ /inning/ ? 'inning/' : '') + file_name, 'w') do |file|
-          file.write(download) 
+          file.write(download)
         end
       end
     end
-  
+
     ##
     # Output a folder format similar to the one used by Gameday.
-    # 
+    #
     # Example:
-    #   >> Spider.format_date_as_folder(Date.new(2011,7,4))
-    #   => "year_2011/month_07/day_04"
-    # 
+    #  >> Spider.format_date_as_folder(Date.new(2011,7,4))
+    #  => "year_2011/month_07/day_04"
+    #
     # Arguments:
-    #   date: (Date)
-  
+    #  date: (Date)
+
     def self.format_date_as_folder(date)
       Greenmonster.format_date_as_folder(date)
     end
